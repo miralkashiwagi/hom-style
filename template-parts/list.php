56b49ccb8dcd0108c26edf9f60f4bfdb1f104e4e
miral_kashiwagi <miral@infoseek.jp>
2025-09-10 23:00:13 +0900

    Update list

--- template-parts/list.php
+++ template-parts/list.php
@@ -1,60 +1,66 @@
 <?php //タクソノミータームまたはauthorページだったら
-if (is_author() || is_tax("category_color")|| is_tax("category_style")) {
+if (is_author() || is_tax("category_color") || is_tax("category_style")) {
     //タイトルを取得
-    add_filter('get_the_archive_title_prefix', function() {
+    add_filter('get_the_archive_title_prefix', function () {
         return '';
     });
     $title = get_the_archive_title();
-    echo "<h2 class='text-md mb-sm font-bold'>".$title."のスタイル一覧</h2>";
+    echo "<h2 class='text-md mb-sm font-bold'>" . $title . "のスタイル一覧</h2>";
 }
 //キーワード検索結果ページだったら
 if (is_search()) {
     //キーワードを取得
-    echo "<h2 class='text-md mb-sm font-bold'>".get_search_query()."の検索結果</h2>";
+    echo "<h2 class='text-md mb-sm font-bold'>" . get_search_query() . "の検索結果</h2>";
 }
 
 ?>
 
 
-<div class="grid grid-cols-2 gap-xl">
-    <?php  while (have_posts()):
-        the_post();?>
-    <a href="<?php the_permalink();?>" class="block">
-        <div class="aspect-[348/482] rounded-md overflow-hidden">
-            <img src="<?php echo wp_get_attachment_image_url( get_lzb_meta("main-image")['id'], "large");?>" alt="" class="w-full h-full object-cover">
-        </div>
-        <div class="flex flex-col gap-xs">
-            <div class="p-xs pb-0">
-                <p class="text-2xs text-gray-text">
-                    <?php the_author();?>
-                </p>
-                <p class="text-md">
-                    <?php the_title();?>
-                </p>
-            </div>
-            <ul class="flex justify-end-safe flex-wrap gap-2xs text-xs text-gray-text">
-                <?php
-                $tax_colors = get_the_terms(get_the_ID(), "category_color");
-                $tax_styles = get_the_terms(get_the_ID(), "category_style");
-                ?>
+    <div class="grid grid-cols-2 gap-xl">
+        <?php
+        $post_count = 0; // カウンターを初期化
+        while (have_posts()):
+            the_post();
+            $post_count++; // カウンターをインクリメント
+            ?>
+            <a href="<?php the_permalink(); ?>" class="block">
+                <div class="aspect-[348/482] rounded-md overflow-hidden">
+                    <img src="<?php echo wp_get_attachment_image_url(get_lzb_meta("main-image")['id'], "medium_large"); ?>"
+                         alt="" class="w-full h-full object-cover"<?php echo ($post_count > 4) ? ' loading="lazy"' : 'fetchpriority="high"'; ?>
+                    >
+                </div>
+                <div class="flex flex-col gap-xs">
+                    <div class="p-xs pb-0">
+                        <p class="text-2xs text-gray-text">
+                            <?php the_author(); ?>
+                        </p>
+                        <p class="text-md">
+                            <?php the_title(); ?>
+                        </p>
+                    </div>
+                    <ul class="flex justify-end-safe flex-wrap gap-2xs text-xs text-gray-text">
+                        <?php
+                        $tax_colors = get_the_terms(get_the_ID(), "category_color");
+                        $tax_styles = get_the_terms(get_the_ID(), "category_style");
+                        ?>
 
-                <?php if (!empty($tax_colors)): ?>
-                    <?php foreach ($tax_colors as $tax_color): ?>
-                        <li>
-                            <span class="block rounded-full bg-gray-bg px-xs py-2xs"><?php echo $tax_color->name;?></span>
-                        </li>
-                    <?php endforeach; ?>
-                <?php endif; ?>
-                <?php if (!empty($tax_styles)): ?>
-                    <?php foreach ($tax_styles as $tax_style): ?>
-                        <li>
-                            <span class="block rounded-full bg-gray-bg px-xs py-2xs"><?php echo $tax_style->name;?></span>
-                        </li>
-                    <?php endforeach; ?>
-                <?php endif; ?>
-            </ul>
-        </div>
-    </a>
-    <?php endwhile; ?>
-</div>
-<?php tailwind_posts_pagination();?>
+                        <?php if (!empty($tax_colors)): ?>
+                            <?php foreach ($tax_colors as $tax_color): ?>
+                                <li>
+                                    <span class="block rounded-full bg-gray-bg px-xs py-2xs"><?php echo $tax_color->name; ?></span>
+                                </li>
+                            <?php endforeach; ?>
+                        <?php endif; ?>
+                        <?php if (!empty($tax_styles)): ?>
+                            <?php foreach ($tax_styles as $tax_style): ?>
+                                <li>
+                                    <span class="block rounded-full bg-gray-bg px-xs py-2xs"><?php echo $tax_style->name; ?></span>
+                                </li>
+                            <?php endforeach; ?>
+                        <?php endif; ?>
+                    </ul>
+                </div>
+            </a>
+        <?php endwhile; ?>
+    </div>
+<?php tailwind_posts_pagination(); ?>
